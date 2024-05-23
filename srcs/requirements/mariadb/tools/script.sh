2d898d79a7256619090027a1b25ac745eb13519d
ffons-ti <ffons-ti@ffons-ti.42.fr>
2024-05-23 16:12:46 +0200

    It works

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,10 +2,13 @@
 
 mysql_install_db
 
-echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > init.sql
+echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> init.sql
+echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> init.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> init.sql
 echo "FLUSH PRIVILEGES;" >> init.sql
+
+chmod +x init.sql
+mv init.sql /etc/mysql/init.sql
 
 mysqld
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 cd /var/www/html
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+mv /wp-config.php /var/www/html/wp-config.php
+
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
+
+
+mv /wp-config.php /var/www/html/wp-config.php
+
+sed -i -r "s/db1/$db_name/1"   wp-config.php
+sed -i -r "s/user/$db_user/1"  wp-config.php
+sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
+
+./wp-cli.phar config create --dbname=wordpress --dbuser=$db_user --dbpass=$db_pwd --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --allow-root
 ./wp-cli.phar user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --alow-root
 ./wp-cli.phar theme install astra --activate --allow-root
